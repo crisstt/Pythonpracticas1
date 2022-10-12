cde848e74f51c1d7a4fb5b35248e65356b646fe7
crisstt <cjrosadod@gmail.com>
2022-10-11 21:10:48 -0500

    v 1.1
    Listas e ingreso de datos

---
+++ 0002 Listas.py
@@ -0,0 +1,37 @@
+print("trabajo con listas python")
+print(25 * "*")
+myList = []
+backList =["YouTube",55,"FaceBook", 68,"Blogg","Vida","Sociedad"]
+myList = backList
+print(50 * "*")
+print(myList [4])
+print(myList [0])
+print(50 * "*")
+print(myList)
+print(50 * "*")
+
+
+'''
+Actualizacion de datos de una lista
+'''
+
+print ("Actualizacion de lista")
+myList [2] = "Irrelevante"
+print(myList)
+
+print(myList [:-1])
+print(myList [::-1])
+print(myList [::2])
+print (myList [:6:1])
+print (myList [1:6:2])
+
+del myList [::2]
+
+print(myList)
+
+
+
+
+
+
+
